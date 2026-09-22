cce3b1f92afc1b8aaba35860a08623de97b3799f
ichirou2910 <ichiroukeita2910@gmail.com>
2026-09-22 23:05:36 +0700

    commit

--- src/spawn/launch.ts
+++ src/spawn/launch.ts
@@ -47,7 +47,7 @@ export function buildWorkerArgv(opts: {
 	const args = [
 		...pi.baseArgs,
 		"--no-extensions",
-		"--extension", "/home/ichirou/Dev/vendor/pi-free",
+		"--extension", "/home/ichirou/Dev/vendor/pi-zen/index.ts",
 		"--no-skills",
 		"--no-prompt-templates",
 		"--no-context-files",
